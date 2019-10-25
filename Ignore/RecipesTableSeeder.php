126bc80b0b4a862db2360638a639180647c68ae2
Unknown <levi.nauwelaerts@gmail.com>
2019-10-25 23:42:58 +0200

    t

--- Ignore/RecipesTableSeeder.php
+++ Ignore/RecipesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
   use Illuminate\Database\Seeder;
 
-  class RecipiestableSeeder extends Seeder
+  class RecipestableSeeder extends Seeder
   {
     public function run(){
     factory(\App\Recipe::class, 5)->create();
